f04c2935c90e7fd18a94b95a2cfe0944df672bf7
Rahul-Verma <94426476+rvstarcaptain@users.noreply.github.com>
2022-06-27 23:11:11 +0530

    commit -m "l"

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package com.rahul;
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");
+        System.out.println(args[0]);
+
+
+    }
+}
